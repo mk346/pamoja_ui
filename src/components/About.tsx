fd10934a4aeed71a6eb5d2e9571d2a1fa6fda05d
x-guru <iamaob8@protonmail.com>
2023-07-06 12:09:00 +0300

    added Testimonials.tsx to Home.js confirmed swiperjs version at tailwind.config.js

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,17 +1,25 @@
+// Initialization for ES Users
+import {
+    Animate,
+    initTE,
+} from "tw-elements";
+
 import React from "react";
 import laptop from "../assets/laptop.jpg";
+
+initTE({ Animate });
 
 const About = () => {
     return (
         <div className='w-full bg-white py-16 px-4'>
             <div className='max-w-[1240px]  mx-auto grid md:grid-cols-2'>
-                <img className='w-[500px] mx-auto my-4' src={laptop} alt="laptop" />
+                <img className='w-[500px] mx-auto my-4' src={laptop} alt="laptop" data-te-animation-on-scroll="repeat"/>
                 <div className='flex flex-col justify-center'>
                     <p className='text-orange-500 font-bold'>ABOUT US</p>
                     <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Our Customers Come First</h1>
                     <p> We offer a wide range of services like building communications networks for a company, safeguarding data and information, creating and administering databases, helping troubleshoot problems in computers or mobile devices, ensuring efficiency and security of business information systems.</p>
                 </div>
-              
+
             </div>
         </div>
     )
