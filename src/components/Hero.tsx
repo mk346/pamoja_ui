e4c41efa3ca4b2179dc38649351304d1eac01e45
x-guru <iamaob8@protonmail.com>
2023-07-27 12:30:12 +0300

    responsive web design on Hero.tsx

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,32 +1,30 @@
-import React from 'react'
-import Typed from 'react-typed'
-import "../styles.css"
+import React from 'react';
+import Typed from 'react-typed';
+import "../styles.css";
 
 const Hero = () => {
-
     return (
         <div className='text-white pb-11 pt-10 header'>
-            <div className='max-w-[800px] mt-[96px] w-full h-[400px] mx-auto text-center flex flex-col justify-center'>
-                <p className='md:text-2xl sm:text-2xl text-orange-500 font-bold mt-5'>Welcome to Pamoja HomeFibre</p>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>The #1 Internet Solutions Provider</h1>
+            <div className='max-w-[800px] mt-12 md:mt-24 w-full h-[300px] md:h-[400px] mx-auto text-center flex flex-col justify-center'>
+                <p className='md:text-xl sm:text-xl text-orange-500 font-bold mt-5'>Welcome to Pamoja HomeFibre</p>
+                <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold md:py-4'>The #1 Internet Solutions Provider</h1>
                 <div className='flex justify-center items-center'>
-                    <p className='md:text-2xl sm:text-2xl text-xl font-bold py-4'>Fast,Flexible and Dependable solutions for</p>
+                    <p className='md:text-xl sm:text-xl text-base font-bold py-2'>We offer </p>
                     <Typed
-                    className='md:text-2xl sm:text-2xl text-xl font-bold md:pl-4 pl-2'
-                        strings={['Fibre internet.','CCTV installation.','IPTV.']}
+                        className='md:text-xl sm:text-xl text-orange-500 font-bold md:pl-2 pl-1'
+                        strings={['Reliable Fibre Internet.', 'CCTV installation.', 'PamojaFlix for Entertainment.']}
                         typeSpeed={50}
                         backSpeed={50}
                         loop
                     />
                 </div>
 
-                <p className='md:text-2xl text-xl font-bold text-white'>We offer fast, reliable and affordable internet connectivity for individual homes or multitenant buildings. </p>
+                <p className='md:text-xl text-base font-bold text-white'> Fast, Reliable, and Affordable Internet connectivity for individual homes or multitenant buildings.</p>
 
-                <button className='bg-orange-500 w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black'>Request Installation</button>
-
+                <button className='bg-orange-500 w-[180px] md:w-[200px] rounded-md font-medium my-6 mx-auto py-2 text-black'>Request Installation</button>
             </div>
         </div>
-    )
-}
+    );
+};
 
 export default Hero;
